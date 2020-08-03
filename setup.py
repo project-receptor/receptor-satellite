9c636515c06c01246641abbf4e786888d050fa9b
Adam Ruzicka <aruzicka@redhat.com>
2020-08-03 13:35:40 +0200

    Bump version to 1.1.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="receptor-satellite",
-    version="1.1.0",
+    version="1.1.1",
     author="Red Hat Ansible",
     url="https://github.com/project-receptor/receptor-satellite",
     license="Apache",
