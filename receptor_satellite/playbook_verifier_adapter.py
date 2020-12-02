fb0a1304de36fc2e4f347dbcf675756e8d13dda1
Adam Ruzicka <aruzicka@redhat.com>
2020-12-02 17:28:12 +0100

    Make linters happy

--- receptor_satellite/playbook_verifier_adapter.py
+++ receptor_satellite/playbook_verifier_adapter.py
@@ -1,7 +1,7 @@
-from insights.client.apps.ansible.playbook_verifier import (
+from insights.client.apps.ansible.playbook_verifier import (  # noqa: F401
     verify,
     PlaybookValidationError,
-)  # noqa: F401
+)
 
 # class PlaybookValidationError(Exception):
 #     pass
