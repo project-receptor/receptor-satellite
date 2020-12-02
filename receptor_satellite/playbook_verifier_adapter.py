59cba697800da0cd1940a169b6e8c0c294239c4f
Adam Ruzicka <aruzicka@redhat.com>
2020-12-02 17:17:49 +0100

    Toggle-able playbook verifier adapter

---
+++ receptor_satellite/playbook_verifier_adapter.py
@@ -0,0 +1,10 @@
+from insights.client.apps.ansible.playbook_verifier import (
+    verify,
+    PlaybookValidationError,
+)  # noqa: F401
+
+# class PlaybookValidationError(Exception):
+#     pass
+
+# def verify(x):
+#     return x
