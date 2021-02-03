e587dfa160f24440a8e6d83b9c3a43ac5758f427
Adam Ruzicka <aruzicka@redhat.com>
2021-02-03 12:03:36 +0100

    Subprocess based playbook signature verification

--- receptor_satellite/playbook_verifier_adapter.py
+++ receptor_satellite/playbook_verifier_adapter.py
@@ -1,12 +1,28 @@
-# from insights.client.apps.ansible.playbook_verifier import (  # noqa: F401
-#     verify,
-#     PlaybookValidationError,
-# )
+import subprocess
 
 
 class PlaybookValidationError(Exception):
     pass
 
 
-def verify(x):
-    return x
+def verify(playbook):
+    try:
+        sub = subprocess.Popen(
+            [
+                "/usr/bin/insights-client",
+                "--module",
+                "insights.client.apps.ansible.playbook_verifier",
+            ],
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+        outs, errs = sub.comunicate(bytes(playbook, "utf-8"))
+        if sub.returncode == 0:
+            return outs.decode("utf-8")
+        else:
+            raise PlaybookValidationError(errs.decode("utf-8"))
+    except OSError:
+        raise PlaybookValidationError("/usr/bin/insights-client not found")
+    except Exception as e:
+        raise PlaybookValidationError(str(e))
